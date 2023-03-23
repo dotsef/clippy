fd78cd5570af786338e48268afcfda163672c952
Sef <severin.fitriyadi@gmail.com>
2023-03-23 13:09:37 +0100

    Clippy first version

---
+++ clippy/Program.cs
@@ -0,0 +1,7 @@
+﻿using TextCopy;
+
+var clipboard = new Clipboard();
+if (Console.IsInputRedirected)
+    clipboard.SetText(Console.In.ReadToEnd());
+else
+    Console.WriteLine(clipboard.GetText());
